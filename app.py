e45690762497051d7c0517531bd0e0e172643e9f
gheacs <gheacs@uw.edu>
2024-01-30 11:02:05 -0800

    create streamlit

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Importing the required libraries
 import sqlite3
+import streamlit as st
+import pandas as pd
+
+# Create Streamlit app
+
+def main():
+    st.title('Student Data')
+    st.write('You can use this app to view the student data.')
+    
+if __name__ == "__main__":
+    main()
+    
