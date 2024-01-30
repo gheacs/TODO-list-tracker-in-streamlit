cc44fd8856bf4eddf93b99379d57f1a7faf6d5d9
gheacs <gheacs@uw.edu>
2024-01-30 12:30:03 -0800

    edit requirement

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-# Importing the required libraries
 import sqlite3
 import streamlit as st
-import pandas as pd
+from pydantic_settings import BaseSettings
+import datetime
+
+# Define a pydantic settings model
+class TaskSettings(BaseSettings):
+    created_at: datetime.datetime
+    created_by: str
+    category: str
+    task_name: str
+    description: str
+    is_done: bool
 
 # Create Streamlit app
-
 def main():
-    st.title('EXPENSE TRACKER')
-    st.write('You can use this app to view the expense tracker.')
-    
+    st.title('TO DO LIST TRACKER')
+    st.write('You can use this app to view the to-do list.')
+
 if __name__ == "__main__":
     main()
-   
+
 # Create a connection to the database
+conn = sqlite3.connect('task_tracker.sqlite3')
+cursor = conn.cursor()  # enable us to run SQL commands
 
-conn = sqlite3.connect('expense_tracker.sqlite3')
-cursor = conn.cursor() # enable us to run SQL commands
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS expense( created_at DATETIME, created_by TEXT, expense_category TEXT, expense_name TEXT, expense_description TEXT, amount FLOAT, is_paid BOOLEAN)''')
-cursor.execute('''INSERT INTO expense VALUES('2021-01-01', 'Ghea', 'Food', 'Lunch', 'Lunch at McDonalds', 10.00, 1)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS task( created_at DATETIME, created_by TEXT, category TEXT, task_name TEXT, description TEXT, is_done BOOLEAN)''')
+cursor.execute('''INSERT INTO task VALUES('2021-10-10 10:00:00', 'Ghea', 'Side Project', 'Send email', 'Send email to doctor', 0)''')
 conn.commit()
 conn.close()
-
