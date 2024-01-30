b0c48208c38300c161357ce48d8e4445212e1cc6
gheacs <gheacs@uw.edu>
2024-01-30 12:12:40 -0800

    revise table

--- app.py
+++ app.py
@@ -6,15 +6,19 @@ import pandas as pd
 # Create Streamlit app
 
 def main():
-    st.title('Student Data')
-    st.write('You can use this app to view the student data.')
+    st.title('EXPENSE TRACKER')
+    st.write('You can use this app to view the expense tracker.')
     
 if __name__ == "__main__":
     main()
    
 # Create a connection to the database
 
-conn = sqlite3.connect('student.sqlite3')
+conn = sqlite3.connect('expense_tracker.sqlite3')
 cursor = conn.cursor() # enable us to run SQL commands
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS student( student_numb INT, name TEXT, undergrad_program TEXT, undergrad_year INT, gpa REAL)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS expense( created_at DATETIME, created_by TEXT, expense_category TEXT, expense_name TEXT, expense_description TEXT, amount FLOAT, is_paid BOOLEAN)''')
+cursor.execute('''INSERT INTO expense VALUES('2021-01-01', 'Ghea', 'Food', 'Lunch', 'Lunch at McDonalds', 10.00, 1)''')
+conn.commit()
+conn.close()
+
