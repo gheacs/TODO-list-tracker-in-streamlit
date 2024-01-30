d5a426cce5694ea87d9579b4c118c233e8c3b6ee
gheacs <gheacs@uw.edu>
2024-01-30 12:42:50 -0800

    edit current timestamp

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ class TaskSettings(BaseSettings):
 def main():
     st.title('TO DO LIST TRACKER')
     st.write('You can use this app to view the to-do list.')
+    
+    
 
 if __name__ == "__main__":
     main()
@@ -24,7 +26,7 @@ if __name__ == "__main__":
 conn = sqlite3.connect('task_tracker.sqlite3')
 cursor = conn.cursor()  # enable us to run SQL commands
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS task( created_at DATETIME, created_by TEXT, category TEXT, task_name TEXT, description TEXT, is_done BOOLEAN)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS task( created_at DATETIME DEFAULT CURRENT_TIMESTAMP, created_by TEXT, category TEXT, task_name TEXT, description TEXT, is_done BOOLEAN)''')
 cursor.execute('''INSERT INTO task VALUES('2021-10-10 10:00:00', 'Ghea', 'Side Project', 'Send email', 'Send email to doctor', 0)''')
 conn.commit()
 conn.close()
